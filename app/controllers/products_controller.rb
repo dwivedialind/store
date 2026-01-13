6f32f957f93230c063de9f1eea94fe96bb26364c
Alind Dwivedi <84173548+dwivedialind@users.noreply.github.com>
2026-01-13 08:29:54 +0530

    Added create method and strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_parmas)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_parmas
+      params.expect(product: [ :name ])
+    end
 end
